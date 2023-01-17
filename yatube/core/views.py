6fae8f63ad0e55a918331fd5fb22285d28dadefe
Никита Седымов <nickitasedymov@yandex.ru>
2023-01-18 00:07:12 +0500

    до вмешательства в модели

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from http import HTTPStatus
+
+from django.shortcuts import redirect, render
 
 
 def page_not_found(request, exception):
-    # Переменная exception содержит отладочную информацию;
-    # выводить её в шаблон пользовательской страницы 404 мы не станем
-    return render(request, 'core/404.html', {'path': request.path}, status=404)
+    return render(
+        request,
+        'core/404.html',
+        {'path': request.path},
+        status=HTTPStatus.NOT_FOUND
+    )
 
 
 def csrf_failure(request, reason=''):
-    return render(request, 'core/403csrf.html')
+    return redirect('core/403csrf.html')
--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Group, Post
+from .models import Comment, Follow, Group, Post
 
 
 @admin.register(Post)
@@ -28,3 +28,26 @@ class GroupAdmin(admin.ModelAdmin):
     )
     search_fields = ('description',)
     list_filter = ('title',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'author',
+        'post',
+        'text',
+        'created',
+    )
+    search_fields = ('text',)
+    list_filter = ('created',)
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'author',
+        'user',
+    )
+    search_fields = ('author__username',)
